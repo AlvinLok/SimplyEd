6503690724eb12618c01f065cd4fdb92f9dc8b0b
ejb1733 <ethanjbillings@gmail.com>
2022-06-15 22:53:19 -0700

    Add functional Google authentication!
    - sign-in and sign-out btns
    - displays Google name & profile picture

--- src/index.js
+++ src/index.js
@@ -31,14 +31,95 @@ import { getPerformance } from 'firebase/performance';
 
 import { getFirebaseConfig } from './firebase-config.js';
 
-const loginbtn = document.querySelector('#loginbtn');
-loginbtn.addEventListener('click', () => console.log('clickd'));
+const signInButtonElement = document.querySelector('#loginbtn');
+const userPicElement = document.querySelector('#user-img');
+const userNameElement = document.querySelector('#user-name');
+const signOutButtonElement = document.querySelector('#logout');
+
+signInButtonElement.addEventListener('click', () => {
+    console.log('clicked sign-in btn');
+    signIn();
+});
+
+signOutButtonElement.addEventListener('click', () => {
+  console.log('sign-out btn clicked!');
+  signOutUser();
+})
 
 const firebaseAppConfig = getFirebaseConfig();
 initializeApp(firebaseAppConfig);
 
+// Signs-in to SimplyEd
+async function signIn() {
+    // Sign in Firebase using popup auth and Google as the identity provider.
+    var provider = new GoogleAuthProvider();
+    await signInWithPopup(getAuth(), provider);
+  }
 
+// Signs-out of SimplyEd
+function signOutUser() {
+  // Sign out of Firebase.
+  signOut(getAuth());
+}
 
+// Initialize firebase auth
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  onAuthStateChanged(getAuth(), authStateObserver);
+}
 
+// Returns the signed-in user's profile Pic URL.
+function getProfilePicUrl() {
+  return getAuth().currentUser.photoURL || '/dist/images/profile_placeholder.png'
+}
 
+// Returns the signed-in user's display name.
+function getUserName() {
+  return getAuth().currentUser.displayName;
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) {
+    // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    var img = document.createElement('img');
+    img.src = profilePicUrl;
+    userPicElement.appendChild(img);
+    // userPicElement.style.backgroundImage =
+    //   'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    userNameElement.textContent = `Welcome to SimplyEd, ${userName}!`;
+
+    // Show user's profile and sign-out button.
+    userNameElement.removeAttribute('hidden');
+    userPicElement.removeAttribute('hidden');
+    signOutButtonElement.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    signInButtonElement.setAttribute('hidden', 'true');
+  } else {
+    // User is signed out!
+    // Hide user's profile and sign-out button.
+    userNameElement.setAttribute('hidden', 'true');
+    userPicElement.setAttribute('hidden', 'true');
+    signOutButtonElement.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    signInButtonElement.removeAttribute('hidden');
+  }
+}
+
+// Adds a size to Google Profile pics URLs.
+function addSizeToGoogleProfilePic(url) {
+  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+    return url + '?sz=150';
+  }
+  return url;
+}
+
+initFirebaseAuth();
 console.log('hello from index.js')
